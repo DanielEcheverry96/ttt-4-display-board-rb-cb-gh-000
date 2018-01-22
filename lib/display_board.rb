b375b5d826cfc1c4c9bc91ee48b8b46eeadb03ae
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-01-22 07:34:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  fila = [" #{board[0]} | #{board[1]} | #{board[2]} ", " #{board[3]} | #{board[4]} | #{board[5]} ", " #{board[6]} | #{board[7]} | #{board[8]} "]
+  espacio = "-----------"
+  puts fila[0]
+  puts espacio
+  puts fila[1]
+  puts espacio
+  puts fila[2]
+end
+
+board = [" ", "O", "X", "O", "X", " ", "X", " ", " "]
+
+display_board(board)
